00412bb5876b15b80186dd07e9b92dc5cfe2ce34
vergik <mihailovpeter@gmail.com>
2021-10-06 19:51:56 +0300

    -

--- app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
@@ -1,7 +1,6 @@
 package com.geekbrains.myfirsttests
 
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Test
 
 class EmailValidatorTest {
@@ -39,5 +38,25 @@ class EmailValidatorTest {
     @Test
     fun emailValidator_NullEmail_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(null))
+    }
+
+    @Test
+    fun emailValidator_Test1() {
+        assertFalse(EmailValidator.isValidEmail("name.com"))
+    }
+
+    @Test
+    fun emailValidator_Test2() {
+        assertFalse(EmailValidator.isValidEmail("name@com"))
+    }
+
+    @Test
+    fun emailValidator_Test3() {
+        assertFalse(EmailValidator.isValidEmail("@@.com"))
+    }
+
+    @Test
+    fun emailValidator_Test4() {
+        assertFalse(EmailValidator.isValidEmail("name@email.name@.email.com"))
     }
 }
